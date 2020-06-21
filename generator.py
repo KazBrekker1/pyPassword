4e7399da653899fd5d5a007a5d95eac8fbf3e901
KazBrekkerSOC <justanothernerd000@gmail.com>
2020-06-21 09:31:31 +0300

    #Basic Password Generator Created

---
+++ generator.py
@@ -0,0 +1,31 @@
+from password_generator import PasswordGenerator
+
+pwd_gen = PasswordGenerator()
+pwd_gen.minuchars = 3
+pwd_gen.minlchars = 3
+pwd_gen.minnumbers = 3
+pwd_gen.minschars = 2
+pwd_gen.excludeschars = ",$._<>%?*"
+
+if __name__ == "__main__":
+    while True:
+        choice = int(input("\n1.Add New / 2.View All / 3.Exit ? "))
+        if choice == 1:
+            with open("output.txt", "a+") as file:
+                pwd_num = input("\nSet Password Length : ")
+                place = input("Website/Application : ")
+                pwd_gen.maxlen = pwd_gen.minlen = int(pwd_num)
+                out = f"{place}:\t{pwd_gen.generate()}\n"
+                print(f"+++++++\n{out}+++++++")
+                file.write(out)
+        elif choice == 2:
+            with open("output.txt", "r") as outfile:
+                print("\n============================\n")
+                for line in outfile:
+                    print(line)
+                print("============================")
+        elif choice == 3:
+            print("\n=======.Thank you.=======\n")
+            break
+        else:
+            print("invalid.")
